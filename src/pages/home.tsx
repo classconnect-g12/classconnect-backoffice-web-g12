0fc634800b0664cd7de6651bb8bbd9c6f00d8365
S2JuanS2 <juansdelrio@hotmail.com>
2025-04-07 17:04:22 -0300

    feat: add Logout

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,16 @@
 import { TabNav } from "@radix-ui/themes";
+import { useNavigate } from "react-router-dom";
+import sessionService from "../services/sessionService";
 
 const Home: React.FC = () => {
+
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    sessionService.clearSession();
+    navigate("/");
+  }
+
   return (
     <>
       <div className="w-1/2 mx-auto">
@@ -10,6 +20,9 @@ const Home: React.FC = () => {
             Admin registration
           </TabNav.Link>
           <TabNav.Link href="#/user-management">User management</TabNav.Link>
+          <TabNav.Link href="#/" onClick={handleLogout}>
+            <span className="text-red-500">Logout</span>
+          </TabNav.Link>
         </TabNav.Root>
       </div>
     </>

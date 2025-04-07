09f29e4fa7add0ee2e61b6f2e9d0f75752a8bee0
S2JuanS2 <juansdelrio@hotmail.com>
2025-04-07 17:00:15 -0300

    feat: add sessionService for handling token in localStorage

---
+++ src/services/sessionService.ts
@@ -0,0 +1,14 @@
+const sessionService = {
+    setSession: (token: string) => {
+      localStorage.setItem("token", token);
+    },
+    getSession: () => {
+      return localStorage.getItem("token");
+    },
+    clearSession: () => {
+      localStorage.removeItem("token");
+    },
+  };
+  
+  export default sessionService;
+  
